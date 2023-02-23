69a8e3608be4029dc49738ee7caab274555dc4e0
Ammar Faiq <12844312+AmmarFaiq@users.noreply.github.com>
2023-02-23 13:01:55 +0100

    change dash_html_components

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 
 import dash
 import dash_core_components as dcc
-import dash_html_components as html
+# import dash_html_components as html
+from dash import html as html
 from dash.dependencies import Input, Output, State
 import dash_daq as daq
 import dash_bootstrap_components as dbc
